b4c88f60a61d5a5379727e691c942ec4deb72cbf
nitin0905 <nk65531@gmail.com>
2022-02-19 22:49:57 +0530

    Updates

---
+++ precache-manifest.127cca961b16286dbf7ace684cb0b0a2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68fb440c796cf2bde90a9cd9da084821",
+    "url": "/WeatherApp/index.html"
+  },
+  {
+    "revision": "059f8ab8f82cfc544778",
+    "url": "/WeatherApp/static/css/main.79e12be6.chunk.css"
+  },
+  {
+    "revision": "277489848103d4f80f3d",
+    "url": "/WeatherApp/static/js/2.9fb7b541.chunk.js"
+  },
+  {
+    "revision": "059f8ab8f82cfc544778",
+    "url": "/WeatherApp/static/js/main.c1024f3a.chunk.js"
+  },
+  {
+    "revision": "4efbdecc212de1558e2a",
+    "url": "/WeatherApp/static/js/runtime~main.3044ef82.js"
+  }
+]);
